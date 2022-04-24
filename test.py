5dbcb0b622dec34c51801033b3d560bd3a805a95
maxmilianand <abc.max@hotmail.com>
2022-04-24 17:59:07 -0300

    Improve performance / Missclicking check

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
 import pyautogui as py
 
 cont = 940
-while True:
-    but = py.locateOnScreen('checkbox.png', confidence=0.85,region=(cont,200,80,900))
-    if but != None:
-        py.click(but)
-        cont += 2
-        print(cont)
+old = 0
+error_count = 0
+for i in range(104):
+    new = py.locateOnScreen('checkbox.png', confidence=0.7,region=(cont,200,80,900))
+    if new == old:
+        error_count += 1
+        print(f'Error1: cant find image on screen. Times ocurred:{error_count}')
+        print(f'Error occured in {new}')
     else:
-        break
-    
+        py.click(new)
+    old = new
+    cont += 2
 
