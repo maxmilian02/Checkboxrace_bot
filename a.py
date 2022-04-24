a466196a791fcf546b77ca5c9f8ac1d53db120d3
maxmilianand <abc.max@hotmail.com>
2022-04-24 17:47:42 -0300

    Cleaning files

--- a.py
+++
@@ -1,4 +0,0 @@
-import pyautogui as py
-from time import sleep
-
-py.screenshot('scre2.png',region=(1106,200,80,900))
--- test2.py
+++
@@ -1,5 +0,0 @@
-import pyautogui as py
-
-
-but = py.locateOnScreen('check2.png', confidence=0.9)
-print(but)
