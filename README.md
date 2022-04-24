917efcf86009cf914c1fe451344454ce0eb03ff7
maxmilianand <abc.max@hotmail.com>
2022-04-24 18:10:53 -0300

    fix

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Checkboxrace_bot
+<h1> Bot for<a href='https://checkboxrace.com/'>checkbox<a></h1>
--- test.py
+++ test.py
@@ -1,20 +1,9 @@
 import pyautogui as py
 
 cont = 940
-old = 0
-error_count = 0
-steps = 0
 for i in range(101):
     new = py.locateOnScreen('checkbox.png', confidence=0.7,region=(cont,200,80,900))
-    if new == old:
-        error_count += 1
-        py.screenshot(f'error_{error_count}_step_{steps}.png',region=(cont,200,80,900))
-        print(f'Error1: cant find image on screen. Times ocurred:{error_count}')
-        print(f'Error occured in {new}')
-    else:
-        py.click(new)
-        steps += 1
-        new = None
-    old = new
+    py.click(new)
+    new = None
     cont += 2
 
