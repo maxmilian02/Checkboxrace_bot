b6120b925d78381a8b5a5355236cb711ca7ea531
maxmilianand <abc.max@hotmail.com>
2022-04-24 18:22:00 -0300

    region corrector

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
 import pyautogui as py
+from time import perf_counter as pf
 
+code_start = pf()
+range1 = 100
 cont = 940
-for i in range(101):
-    new = py.locateOnScreen('checkbox.png', confidence=0.7,region=(cont,200,80,900))
+for i in range(104):
+    new = py.locateOnScreen('checkbox.png', confidence=0.9,region=(cont,200,80,900))
     py.click(new)
     new = None
     cont += 2
+
+print(f'Code ended in {code_start} seconds')
 
